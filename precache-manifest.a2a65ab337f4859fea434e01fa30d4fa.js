c7aae581bcf56068184f3eee293c42bd1279cad5
ji <jkim0107@gmail.com>
2020-01-28 20:23:56 -0800

    Updates

---
+++ precache-manifest.a2a65ab337f4859fea434e01fa30d4fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e53c1c282e265aa3b6dc986a4906d3",
+    "url": "/masterm1nd-game/index.html"
+  },
+  {
+    "revision": "6acc2f1ea25a86983863",
+    "url": "/masterm1nd-game/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "5ade1eb7f36d99313ff4",
+    "url": "/masterm1nd-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6acc2f1ea25a86983863",
+    "url": "/masterm1nd-game/static/js/2.69acebf4.chunk.js"
+  },
+  {
+    "revision": "a7e9e012a3f716fd81cf8afb9ff9cff5",
+    "url": "/masterm1nd-game/static/js/2.69acebf4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ade1eb7f36d99313ff4",
+    "url": "/masterm1nd-game/static/js/main.b1092c04.chunk.js"
+  },
+  {
+    "revision": "59fe020a717477317d10",
+    "url": "/masterm1nd-game/static/js/runtime-main.ce9ae0cf.js"
+  }
+]);
